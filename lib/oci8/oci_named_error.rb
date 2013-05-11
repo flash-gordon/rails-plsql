929ce99416a1f68953825ffae01ed6cec70e71b0
Nikita Shilnikov <ns@latera.ru>
2013-05-12 03:44:17 +0400

    Added handling of ORA-06512

--- lib/oci8/oci_named_error.rb
+++ lib/oci8/oci_named_error.rb
@@ -2,8 +2,14 @@ class OCI8
   class OCINamedError < OCIError
     class_attribute :oci_error_code, instance_writer: false
 
+    UNHANDLED_ERROR = 6512
+
     def self.===(error)
-      OCIError === error && error.code == oci_error_code
+      OCIError === error &&
+          (error.code == oci_error_code ||
+           # ORA-06512: at line 1
+           # ORA-20100: some exception description <--- real excpetion code in the second line
+           error.code == UNHANDLED_ERROR && error.message.split("\n")[1].try(:[], /\AORA-(\d+)/, 1).try(:to_i))
     end
 
     def self.define_exception(class_name, error_code)
